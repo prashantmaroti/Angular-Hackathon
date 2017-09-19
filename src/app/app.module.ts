19ffb175a22659a00e022545bfe4fc6030f82e1f
Akshay Dharamkar <akshay.dharamkar@gmail.com>
2017-09-19 17:26:43 +0530

    Small fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GameComponent } from './game/game.component';
-import { FormComponent } from './form/form.component';
 import { AppRoutingModule } from './app-routing';
 import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent,
-    FormComponent
   ],
   imports: [
     BrowserModule,
